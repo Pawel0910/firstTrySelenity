f914f9565dc4a31a7942101db559d214d7cc2b43
kielan <ja@gmail.com>
2016-02-28 21:08:03 +0100

    dla ostatniBranch

--- src/main/java/com/mysite/selenium/HomePage.java
+++ src/main/java/com/mysite/selenium/HomePage.java
@@ -1,7 +1,11 @@
 package com.mysite.selenium;
 
+import org.junit.Test;
 import org.openqa.selenium.By;
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static junit.framework.TestCase.assertTrue;
 /**
  * Created by Kielan on 24.02.2016.
  */
@@ -16,4 +20,24 @@ public class HomePage {
 
     // download link
     public static final By LOC_LNK_DOWNLOADSELENIUM = By.linkText("Download Selenium");
+
+    @Test
+    public void startWebDriver(){
+        //WebDriver driver = new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("http://localhost:4567");
+        assertTrue(driver.getTitle().startsWith("Free"));
+        System.out.println("działa?");
+        WebElement email = driver.findElement(By.id("email"));
+        email.sendKeys("23");
+        WebElement pass = driver.findElement(By.id("pwd"));
+        pass.sendKeys("1");
+
+        driver.findElement(By.id("zalogujButton")).click();
+        String napis = driver.findElement(By.id("witaj")).getText();
+        System.out.println(napis+" ciekawe");
+        driver.close();
+        driver.quit();
+    }
 }
